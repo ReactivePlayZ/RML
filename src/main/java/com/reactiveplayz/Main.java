3fc753aa6d2c5c60058957ec0bf01cc9375c99b2
ReactivePlayZ <reactiveplayz@gmail.com>
2025-12-13 22:15:06 +0600

    Refactor main method to handle command-line arguments and improve help message functionality

--- src/main/java/com/reactiveplayz/Main.java
+++ src/main/java/com/reactiveplayz/Main.java
@@ -1,7 +1,6 @@
 package com.reactiveplayz;
 
 import java.io.File;
-import java.util.Scanner;
 
 public class Main {
     private static File rmlFile;
@@ -19,13 +18,72 @@ public class Main {
          * System.out.println(i + "# | " + comments.get(i));
          * }
          */
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.print("Enter a (RML) file path: ");
-            rmlFile = new File(scanner.nextLine());
-        } while (!rmlFile.exists() || !rmlFile.isFile());
-        scanner.close();
-        Parser.Parse(rmlFile);
-        Converter.write(rmlFile);
+        if (args.length == 0) {
+            helpMsg();
+            System.exit(0);
+        }
+        if (args.length == 1) {
+            if (args[0].toLowerCase().equals("help") || args[0].toLowerCase().equals("--help")
+                    || args[0].equals("/?")) {
+                helpMsg();
+                System.exit(0);
+            }
+            if (args[0].toLowerCase().equals("--createjson")) {
+                helpMsg("--createjson");
+                System.exit(1);
+            }
+            if (!args[0].startsWith("--")) {
+                System.out.println("Commands and flags must start with --");
+                System.exit(1);
+            }
+            helpMsg();
+        }
+        if (args.length >= 2) {
+            if (args[0].toLowerCase().equals("help") || args[0].toLowerCase().equals("--help")) {
+                helpMsg(args[1]);
+                System.exit(0);
+            }
+            if (args[0].toLowerCase().equals("--createjson")) {
+                rmlFile = new File(args[1]);
+                if (!rmlFile.exists() || !rmlFile.isFile()) {
+                    System.out.println("The given path does not exist or is not a file: " + rmlFile.getAbsolutePath());
+                    System.exit(1);
+                }
+                Parser.Parse(rmlFile);
+                Converter.write(rmlFile);
+            }
+        }
+    }
+
+    public static void helpMsg() {
+        System.out.println("RML Interpreter:");
+        helpMsg("--help");
+        helpMsg("--createjson");
+
+    }
+
+    public static void helpMsg(String command) {
+        command = command.toLowerCase();
+        switch (command) {
+            case "--help", "help":
+                System.out.println("--help or help:");
+                System.out.println("    Without arguments prints all command usages");
+                System.out.println("    With a command argument prints that command's help message:");
+                System.out.println("    --help (--command)");
+                System.out.println();
+                break;
+            case "--createjson", "createjson":
+                System.out.println("--createJson:");
+                System.out.println("    Converts the RML file to JSON");
+                System.out.println("    --createJson <path to file>");
+                System.out.println();
+                break;
+            case "--output", "output":
+                System.out.println("--output:");
+                System.out.println("    Not implemented yet but will allow to specify output directory");
+                break;
+            default:
+                System.out.println("no command found");
+        }
     }
 }
